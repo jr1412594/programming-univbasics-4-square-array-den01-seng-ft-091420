b382f68fc1f179cf11fa857e3a6371f4a7cc7ab7
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-05 19:28:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   
   while count < array.length do
-     new_array.push(array[count] * [i])
+     new_array.push(array[count] * array[count])
      count +=1 
   end
   return new_array
